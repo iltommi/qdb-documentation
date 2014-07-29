da50f91bbd2539dc3a4138c76f95248b2f1bb531
Andrew Dorney <andrewd18@gmail.com>
2014-07-28 22:40:06 -0500

    Add auto-generated Java documentation to the repo.  * Requires Python package "javasphinx" installed, 0.9.12 or higher (unreleased as of 2014-07-29)  * Requires javasphinx-apidoc to be in the system path.  * Expects the ./qdb/com/b14/qdb/ folder to contain the java source code (subfolders beyond there are ok).

--- source/conf.py
+++ source/conf.py
@@ -25,7 +25,7 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.intersphinx', 'sphinx.ext.todo', 'sphinx.ext.mathjax', 'sphinx.ext.autodoc']
+extensions = ['sphinx.ext.intersphinx', 'sphinx.ext.todo', 'sphinx.ext.mathjax', 'sphinx.ext.autodoc', 'javasphinx']
 
 # crawl back to where the API is being built
 sys.path.insert(0, os.path.join(os.getcwd(), '..', 'qdb', 'api', 'python'))
@@ -272,4 +272,3 @@ epub_copyright = u'2013, Bureau 14 SARL'
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'http://docs.python.org/': None}
-
