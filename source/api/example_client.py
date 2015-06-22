09ca696b6a9b046bfa0f331c04d54b0a7ba90655
Andrew Dorney <andrewd18@gmail.com>
2015-06-21 20:15:49 -0500

    Update Python API page to 2.0.0beta3.

--- source/api/example_client.py
+++ source/api/example_client.py
@@ -2,16 +2,18 @@ import qdb
 
 # Assuming we have a quasardb server running on dataserver.mydomain:3001
 # Note this will throw an exception if the quasardb cluster is not available.
-cl = qdb.Client("qdb://dataserver.mydomain:3001")
+cl = qdb.Cluster("qdb://dataserver.mydomain:3001")
 
 # We want to silently create or update the object
 # depending on the existence of the key in the cluster.
 def save(key, obj):
-    cl.update(key, obj)
+    b = cl.blob(key)
+    b.update(obj)
 
 # We want to simply return None if the key is not found in the cluster.
 def load(key):
     try:
-        return cl.get(key)
+        b = cl.blob(key)
+        return b.get(key)
     except qdb.QuasardbException:
         return None
