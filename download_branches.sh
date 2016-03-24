44a2bbf4c1637fe7e79e65666f1f24f77fc73619
Benoit Blanchon <benoit@quasardb.net>
2016-03-24 11:45:04 +0000

    Call git pull

--- download_branches.sh
+++ download_branches.sh
@@ -2,19 +2,21 @@
 
 cd $(dirname $0)
 
+git pull
+
 ORIGIN=$(git config --get remote.origin.url)
 BRANCHES=$(git branch --list --remotes origin/* | grep -E 'origin/[0-9]+.[0-9]+.[0-9]' | sed 's|origin/||' | sort --version-sort)
 
-# for BRANCH in $BRANCHES; do
-#     if [ -e "source/$BRANCH/.git" ]; then
-#         echo "Pulling branch $BRANCH"
-#         git -C "source/$BRANCH" pull --quiet
-#     else
-#         echo "Cloning branch $BRANCH"
-#         git clone --quiet --depth 1 --branch $BRANCH $ORIGIN "source/$BRANCH"
-#     fi
-#     LATEST=$BRANCH
-# done
+for BRANCH in $BRANCHES; do
+    if [ -e "source/$BRANCH/.git" ]; then
+        echo "Pulling branch $BRANCH"
+        git -C "source/$BRANCH" pull --quiet
+    else
+        echo "Cloning branch $BRANCH"
+        git clone --quiet --depth 1 --branch $BRANCH $ORIGIN "source/$BRANCH"
+    fi
+    LATEST=$BRANCH
+done
 
 (    
     export BRANCHES=$(echo ${BRANCHES[*]})
