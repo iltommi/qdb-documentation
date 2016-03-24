938b5295dfbdb129c9a00d53388b9e26d726681f
Benoit Blanchon <benoit@quasardb.net>
2016-03-24 11:21:25 +0000

    Update versions in .js file

--- download_branches.sh
+++ download_branches.sh
@@ -1,19 +1,29 @@
 #!/bin/bash -eu
 
+cd $(dirname $0)
+
 ORIGIN=$(git config --get remote.origin.url)
 BRANCHES=$(git branch --list --remotes origin/* | grep -E 'origin/[0-9]+.[0-9]+.[0-9]' | sed 's|origin/||' | sort --version-sort)
 
-for BRANCH in $BRANCHES; do
-    if [ -e "source/$BRANCH/.git" ]; then
-        echo "Pulling branch $BRANCH"
-        git -C "source/$BRANCH" pull --quiet
-    else
-        echo "Cloning branch $BRANCH"
-        git clone --quiet --depth 1 --branch $BRANCH $ORIGIN "source/$BRANCH"
-    fi
-    LATEST=$BRANCH
-done
+# for BRANCH in $BRANCHES; do
+#     if [ -e "source/$BRANCH/.git" ]; then
+#         echo "Pulling branch $BRANCH"
+#         git -C "source/$BRANCH" pull --quiet
+#     else
+#         echo "Cloning branch $BRANCH"
+#         git clone --quiet --depth 1 --branch $BRANCH $ORIGIN "source/$BRANCH"
+#     fi
+#     LATEST=$BRANCH
+# done
 
-echo "Generate index.html"
-export LATEST
-envsubst <source/index.html.in >source/index.html
+(    
+    export BRANCHES=$(echo ${BRANCHES[*]})
+    export LATEST
+    
+
+    echo "Generate index.html"
+    envsubst <source/index.html.in >source/index.html
+    
+    echo "Generate version_switch.html"
+    envsubst <source/shared/_static/version_switch.js.in >source/shared/_static/version_switch.js
+)
