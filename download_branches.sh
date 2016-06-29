0b962fda1c6831911ce24c45f680a4701095a4af
Benoit Blanchon <benoit@quasardb.net>
2016-06-29 14:29:03 +0200

    Don't use "sort --version-sort" as it's not available on OSX

--- download_branches.sh
+++ download_branches.sh
@@ -5,7 +5,7 @@ cd $(dirname $0)
 git fetch
 
 ORIGIN=$(git config --get remote.origin.url)
-BRANCHES=$(git branch --list --remotes origin/* | grep -E 'origin/[0-9]+.[0-9]+.[0-9]' | sed 's|origin/||' | sort --version-sort)
+BRANCHES=$(git branch --list --sort=version:refname --remotes origin/* | grep -E 'origin/[0-9]+.[0-9]+.[0-9]' | sed 's|origin/||')
 
 for BRANCH in $BRANCHES; do
     if [ -e "source/$BRANCH/.git" ]; then
@@ -18,14 +18,14 @@ for BRANCH in $BRANCHES; do
     LATEST=$BRANCH
 done
 
-(    
+(
     export BRANCHES=$(echo ${BRANCHES[*]})
     export LATEST
-    
+
 
     echo "Generate index.html"
     envsubst <source/index.html.in >source/index.html
-    
+
     echo "Generate version_switch.html"
     envsubst <source/shared/_static/version_switch.js.in >source/shared/_static/version_switch.js
 )
