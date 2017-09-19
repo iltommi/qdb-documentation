793eff14f1aeeac2424a82b807f5591df8020baf
Marek Kurdej <marek@quasardb.net>
2017-09-19 14:36:36 +0200

    Update Python API.

--- qdb/python/quasardb/impl.py
+++ qdb/python/quasardb/impl.py
@@ -1,4 +1,4 @@
 # Dummy implementation file
 #
-# We just want Sphinx to be able to load qdb/__init__.py
-# and we don't care if the code doesn't accually works
+# We just want Sphinx to be able to load __init__.py
+# and we don't care if the code doesn't actually works.
