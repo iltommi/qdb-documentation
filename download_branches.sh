a090ad9a947b3b5cb0556dd1902ddceb351454c0
Marek Kurdej <marek@quasardb.net>
2017-11-24 11:26:42 +0100

    Version selector shows branch names from conf.py if possible.
    Format version_switch.js.

--- download_branches.sh
+++ download_branches.sh
@@ -16,6 +16,7 @@ fi
 
 echo 'Branches:' $BRANCHES
 
+declare -a BRANCH_NAMES
 for BRANCH in $BRANCHES; do
     if [ -e "source/$BRANCH/.git" ]; then
         echo "Pulling branch $BRANCH"
@@ -24,16 +25,23 @@ for BRANCH in $BRANCHES; do
         echo "Cloning branch $BRANCH"
         git clone --quiet --depth 1 --branch $BRANCH $ORIGIN "source/$BRANCH"
     fi
+
+    BRANCH_NAME=${BRANCH}
+    if [ -e "source/$BRANCH/source/conf.py" ] ; then
+        BRANCH_NAME=$(python -c "execfile('source/${BRANCH}/source/conf.py') ; print(release)")
+    fi
+    BRANCH_NAMES+=" ${BRANCH_NAME}"
     LATEST=$BRANCH
 done
 
 (
-    export BRANCHES=$(echo ${BRANCHES[*]})
+    # Do not use underscores in variables passed to envsubst!
+    export BRANCHES=$(echo ${BRANCH_NAMES[*]})
     export LATEST
 
     echo "Generate index.html"
-    envsubst <source/index.html.in >source/index.html
+    envsubst \$LATEST <source/index.html.in >source/index.html
 
     echo "Generate version_switch.html"
-    envsubst <source/shared/_static/version_switch.js.in >source/shared/_static/version_switch.js
+    envsubst \$BRANCHES <source/shared/_static/version_switch.js.in >source/shared/_static/version_switch.js
 )
